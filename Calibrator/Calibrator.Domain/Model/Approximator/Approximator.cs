c0fa441d822367b7fc5372bbb299899f8e3e4a47
TApocEK111 <realms1337@rambler.ru>
2024-07-03 22:07:08 +0300

    approximation and results in channel

--- Calibrator/Calibrator.Domain/Model/Approximator/Approximator.cs
+++ Calibrator/Calibrator.Domain/Model/Approximator/Approximator.cs
@@ -1,16 +1,25 @@
-﻿namespace Calibrator.Domain.Model.Approximator;
+﻿using Calibrator.Domain.Model.Report;
+
+namespace Calibrator.Domain.Model.Approximator;
 
 public static class Approximator
 {
     static double[] coefficients;
 
-    static Approximator()
-    {
+    public static void CalculateCoefficients(SensorChannel channel) => coefficients = Matrix.GetCoeffitients(channel);
 
+    public static double Aproximate(double parameter)
+    {
+        return coefficients[0] * parameter * parameter + coefficients[1] * parameter + coefficients[2];
     }
 
-    public static void AproximateResults(Report.SensorChannel channel)
+    public static void CalculateResults(SensorChannel channel)
     {
-        
+        var results = new Dictionary<Experiment, double>();
+        foreach (var parameter in channel.Parameters)
+        {
+            results.Add(parameter.Key, Aproximate(parameter.Value));
+        }
+        channel.Results = results;
     }
 }
--- Calibrator/Calibrator.Tests/ApproximatorTests.cs
+++ Calibrator/Calibrator.Tests/ApproximatorTests.cs
@@ -6,7 +6,7 @@ namespace Calibrator.Tests
 
         public void ApproximatesValuesCorrectly(Dictionary<Experiment, double> expected, SensorChannel channel)
         {
-            Approximator.AproximateResults(channel);
+            Approximator.Aproximate(channel);
 
             Assert.Equal(expected, channel.Results);
         }
