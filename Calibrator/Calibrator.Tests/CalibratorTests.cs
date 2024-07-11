e48ce5f670b14218d5f8b754009bf256553f71e9
Cucmbr <vanvanich531@gmail.com>
2024-07-11 11:18:58 +0300

    Update CalibratorTests.cs

--- Calibrator/Calibrator.Tests/CalibratorTests.cs
+++ Calibrator/Calibrator.Tests/CalibratorTests.cs
@@ -3,12 +3,18 @@ namespace Calibrator.Tests
     public class CalibratorTests
     {
         [Fact]
-        public void CalibratesCorrectly(double[] references, double[] parameters)
+        public void CalibratesCorrectly()
         {
             var testHelper = new TestHelper();
             var report = testHelper.TestReport;
 
             var calibrator = new Domain.Model.Calibrator.Calibrator();
+            calibrator.CalculatePhysicalQuantitylValues(report);
+
+            foreach (var sample in report.Sensors[0].Channels[0].Samples)
+            {
+                Assert.Equal(report.Sensors);
+            }
 
         }
     }
